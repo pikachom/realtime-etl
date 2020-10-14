73f572d772ad8bf8c2d4576c7439487495d67cfd
byeongeui_jung <byeongeui_jung@tmax.co.kr>
2020-10-14 18:53:27 +0900

    Kafka - Spark - Mongo connection init

--- spark-transform/src/main/scala/spark/MongoSparkMain.scala
+++ spark-transform/src/main/scala/spark/MongoSparkMain.scala
@@ -1,42 +1,80 @@
 package spark
 
 
-import com.mongodb.spark.sql._
-import com.mongodb.spark.config.ReadConfig
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.SparkSession
+import com.mongodb.client.MongoCollection
+import com.mongodb.spark.MongoConnector
+import com.mongodb.spark.config.{ReadConfig, WriteConfig}
+import org.apache.spark.sql.ForeachWriter
 
-object MongoSparkMain extends ConfigHelper {
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+import org.bson.Document
+
+object MongoSparkMain { // extends ConfigHelper {
   val kafkaUri = "localhost:9092"
   val kafkaTopic = "test-topic-1"
   val kafkaGroupId = "test"
 
-  val mongoUri = "mongodb://localhost:27017/test"
+  val mongoUri = "mongodb://localhost:27017"
 
   def main(args: Array[String]): Unit = {
+    import org.apache.spark.sql.SparkSession
+
     val spark = SparkSession
       .builder
       .appName("spark-transform")
       .master("local[*]")
       .config("spark.mongodb.input.uri", mongoUri)
       .config("spark.mongodb.output.uri", mongoUri)
+      .config("spark.mongodb.output.database", "local")
+      .config("spark.mongodb.output.collection", "po_log")
       .getOrCreate()
-
 
     import spark.implicits._
 
-    val kafka_df = spark
+    val kafkaDf = spark
       .readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", kafkaUri)
-      .option("subscribe", kafkaTopic)
       .option("group.id", kafkaGroupId)
+      .option("subscribe", kafkaTopic)
       .load()
-    kafka_df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-      .as[(String, String)]
-    kafka_df.printSchema()
 
-    kafka_df.write.mode("append").mongo()
+    val logDf = kafkaDf.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)").as[(String, String)]
 
+    /*
+    // print logs on console for debugging
+    val consoleOutput = logDf.writeStream
+      .outputMode("append")
+      .format("console")
+      .start()
+    consoleOutput.awaitTermination()
+    */
+
+    val query = logDf.writeStream.outputMode("append")
+      .foreach(new ForeachWriter[(String, String)] {
+        val writeConfig: WriteConfig = WriteConfig(Map("uri" -> "mongodb://localhost:27017/local.po_log"))
+        var mongoConnector: MongoConnector = _
+        var logs: mutable.ArrayBuffer[(String, String)] = _
+
+        override def open(partitionId: Long, epochId: Long): Boolean = {
+          mongoConnector = MongoConnector(writeConfig.asOptions)
+          logs = new mutable.ArrayBuffer[(String, String)]()
+          true
+        }
+
+        override def process(value: (String, String)): Unit = {
+          logs.append(value)
+        }
+
+        override def close(errorOrNull: Throwable): Unit = {
+          if (logs.nonEmpty) {
+            mongoConnector.withCollectionDo(writeConfig, {collection: MongoCollection[Document] =>
+              collection.insertMany(logs.map(log => { new Document(log._1, log._2)}).asJava)
+            })
+          }
+        }
+      }).start()
+    query.awaitTermination()
   }
 }
